a1fb8710931b2f615e61430bff2e320e12839aa8
heintzz <mhasnanr@gmail.com>
2023-05-01 19:24:42 +0700

    feat: add login api

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 import { auth } from './firebase'
 
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
 
 export async function signUp(user) {
   const { email, password } = user
@@ -10,7 +10,23 @@ export async function signUp(user) {
     const response = { success: true, msg: `user with email: ${user.email} created` }
     return response
   } catch (error) {
-    const errorMessage = error.code
+    let errorMessage = error.code
+    errorMessage = errorMessage.substring('auth/'.length, errorMessage.length)
+    const response = { success: false, msg: errorMessage }
+    return response
+  }
+}
+
+export async function signIn(user) {
+  const { email, password } = user
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password)
+    const user = userCredential.user
+    const response = { success: true, msg: `user with email: ${user.email} has logged in`, token: user.accessToken }
+    return response
+  } catch (error) {
+    let errorMessage = error.code
+    errorMessage = errorMessage.substring('auth/'.length, errorMessage.length)
     const response = { success: false, msg: errorMessage }
     return response
   }
